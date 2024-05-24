bfa97dd722c63ffa39caf8d9ece4e52ae0cb27ec
Rafaela Curti de Paula <70167337+RafaelaCurti@users.noreply.github.com>
2024-05-24 17:50:00 -0300

    Merge

---
+++ Areas/my-app/src/components/layout/default/footer/Index.style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Cabecalho = styled.div`
+  background: #000000;
+  color: #F7F8F7;
+  padding: 16px;
+  font-size: 0.8em;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+`;
---
+++ Areas/my-app/src/components/layout/default/header/Index.style.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Cabecalho = styled.div`
+  background: #009640;
+  color: #F7F8F7;
+  padding: 16px;
+  height: 80px;
+`;
+
+export const AreaMenu = styled.div`
+  background: #F7F8F7fff;
+  color: #000000;
+  height: 200px;
+`;
+
+export const Logo = styled.img`
+  width: 100px;
+  height: 50px;
+`;
+
+export const EncerrarSessao = styled.div`
+  text-align: right;
+`;
